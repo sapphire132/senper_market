18306b80687e19cdaed7a92a9e2fa977a0889382
sapphire132 <gemg2019@gmail.com>
2020-09-06 23:54:23 +0300

    configure model for tutorial

--- tutorials/models.py
+++ tutorials/models.py
@@ -5,6 +5,7 @@ class Tutorial(models.Model):
     course_name = models.CharField(max_length=100)
     course = models.CharField(max_length=200)
     date = models.DateTimeField(default=datetime.now, blank=True)
+    thumbnail = models.ImageField(upload_to = 'thumbnails/%Y/%m/%d/')
 
     def __str__(self):
         return self.course_name
