43ecd15492bfa973ac560204d9df46561b990b13
sapphire132 <gemg2019@gmail.com>
2020-09-08 23:32:53 +0300

    added local settings

--- sapphire/settings.py
+++ sapphire/settings.py
@@ -140,3 +140,9 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'gemg2019@gmail.com'
 EMAIL_HOST_PASSWORD = '#pass@gmemg2019'
 EMAIL_USE_TSL = True
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
